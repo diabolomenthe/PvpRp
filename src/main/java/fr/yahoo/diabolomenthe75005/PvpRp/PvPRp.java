5083d7234da4bb5a9e6a5b8271609c594e12fc06
diabolomenthe75005 <diabolomenthe75005@yahoo.fr>
2014-08-17 11:18:42 +0200

    Création projet

---
+++ src/main/java/fr/yahoo/diabolomenthe75005/PvpRp/PvPRp.java
@@ -0,0 +1,21 @@
+package fr.yahoo.diabolomenthe75005.PvpRp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public class PvPRp extends JavaPlugin{
+	//Fonction lors du lancement du serveur
+		@Override
+		public void onEnable(){
+			
+
+		}
+
+
+		//Fonction lors de l'arrêt du serveur
+		@Override
+		public void onDisable(){
+
+		}
+}
